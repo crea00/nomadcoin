ee33205d50e065239f4e33fe76086521c8177e5d
crea <654321su@gmail.com>
2018-07-09 08:58:52 +0900

    #04 Creating the Genesis Block

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,16 @@ class Blcok {
     this.data = data;
   }
 }
+
+// The genesis block has to be hardcoded, so we have to type!
+const genesisBlock = new Blcok(
+  0,
+  "E5B9D14256F1684580288FECDEFB5095390D901A43AC7C44428E59EEB5A84C18",
+  null,
+  1531094133.16,    //new Date().getTime / 1000
+  "This is the genesis!!"
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain);
