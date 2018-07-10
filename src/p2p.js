1fea4b9fc388e663ec0a1fc7ac21980ab928d3d2
crea00 <654321su@gmail.com>
2018-07-10 20:59:02 +0900

    #16 Sending Messages Between Peers

--- src/p2p.js
+++ src/p2p.js
@@ -9,6 +9,7 @@ const getSockets = () => sockets;
 const startP2PServer = server => {
   const wsServer = new WebSockets.Server({ server });
   wsServer.on("connection", ws => {
+    initSocketConnection(ws);
     console.log(`Hello Socket`);
   });
   console.log("Nomadcoin P2P Server Running!");
@@ -16,6 +17,12 @@ const startP2PServer = server => {
 
 const initSocketConnection = socket => {
   sockets.push(socket);
+  socket.on("message", (data) => {
+    console.log(data);
+  });
+  setTimeout(() => {
+    socket.send("welcome");
+  }, 5000);
 };
 
 const connectToPeers = newPeer => {
