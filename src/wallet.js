92edf54a9e0047642324b2f4d40154a9f6c4c6ec
crea00 <654321su@gmail.com>
2018-08-24 08:55:09 +0900

    #70 Filtering uTxOuts that are already in the Mempool

--- src/wallet.js
+++ src/wallet.js
@@ -75,6 +75,29 @@ const createTxOuts = (receiverAddress, myAddress, amount, leftOverAmount) => {
   }
 };
 
+const filterUTxOutsFromMempool = (uTxOutList, mempool) => {
+
+  const txIns = _(mempool)
+    .map(tx => tx.txIns)
+    .flatten()
+    .values();
+
+    const removables = [];
+
+  for(const uTxOut of uTxOutList) {
+    const txIn = _.find(
+      txIns, txIn => 
+        txIn.txOutIndex === uTxOut.txOuIndex && 
+        txIn.txOutId === uTxOut.txOutId
+    );
+    if(!txIn === undefined) {
+      removables.push(txIn);
+    }
+  }
+
+  return _.without(uTxOutList, ...removables);
+};
+
 const createTx = (receiverAddress, amount, privateKey, uTxOutList) => {
   const myAddress = getPublicKey(privateKey);
   const myUTxOuts = uTxOutList.filter(uTxO => uTxO.address === myAddress);
